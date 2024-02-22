2a3a92bd0d81832d1c6ea6e3f39b0edf8bb2a088
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-02-22 19:28:53 +0530

    ⏳🔄 Added page loader

--- script.js
+++ script.js
@@ -1,23 +1,64 @@
-window.onload = function () {
+window.onload = async function () {
+    try {
+        const position = await getPosition();
+        await setAttributesValue(position);
+    } catch (error) {
+        alert(error);
+    }
+
+    // Hide loader and remove blur
+    document.querySelector(".loader-div").style.display = "none";
+    document.getElementById("page-container").classList.remove("blur");
+}
+
+function getPosition() {
+    return new Promise((resolve, reject) => {
+        getCurrentPosition(function (position) {
+            if (position) {
+                resolve(position);
+            } else {
+                reject("Failed to retrieve location.");
+            }
+        });
+    });
+}
+
+async function setAttributesValue(position){
+    let { latitude, longitude } = position;
+    const promises = [
+        setLocationCityName(latitude, longitude),
+        getSunriseSunset(latitude, longitude, getCurrentDateAsString()),
+        setIslamicDate()
+    ];
+    await Promise.all(promises);
+}
+
+function getCurrentPosition(callback) {
     if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(async function (position) {
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
-            setLocationCityName(latitude, longitude);
-            getSunriseSunset(latitude, longitude, getCurrentDateAsString());
-            setIslamicDate();
-        }, function (error) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            let latitude = position.coords.latitude;
+            let longitude = position.coords.longitude;
+            callback({ latitude, longitude });
+
+        }, async (error) => {
             console.error('Error:', error);
-            alert("Failed to retrieve your time.");
+            var locationByIPUrl = 'http://ip-api.com/json'
+            var locationData = await fetchDataFromAPI(locationByIPUrl)
+            const { lat, lon } = locationData;
+            let latitude = lat
+            let longitude = lon;
+            callback({latitude, longitude});
         });
     } else {
         alert("Geolocation is not supported by your browser");
+        callback(null);
     }
 }
+
 async function setLocationCityName(latitude, longitude) {
     var nominatimApiUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`;
     var result = await fetchDataFromAPI(nominatimApiUrl);
-    document.getElementById('location').innerText =  result.address.city + ', ' + result.address.state + ', ' + result.address.country;
+    document.getElementById('location').innerText = result.address.city + ', ' + result.address.state + ', ' + result.address.country;
 }
 
 function setIslamicDate() {
@@ -33,7 +74,7 @@ function getIslamicDate(date) {
         'Muharram', 'Safar', 'Rabi\' al-awwal', 'Rabi\' al-thani', 'Jumada al-awwal', 'Jumada al-thani',
         'Rajab', 'Shaʻban', 'Ramadan', 'Shawwal', 'Dhu al-Qi\'dah', 'Dhu al-Hijjah'
     ];
-    const formattedDate = `${monthsIslamic[parseInt(month)-1]} ${day}, ${yearAndEra}`;
+    const formattedDate = `${monthsIslamic[parseInt(month) - 1]} ${day}, ${yearAndEra}`;
     return formattedDate;
 }
 
@@ -67,7 +108,7 @@ function addMinutesInTime(timeStr, minutes) {
     return resultStr;
 }
 
-function minutesToFormat(totalMinutes){
+function minutesToFormat(totalMinutes) {
     let hours = Math.floor(totalMinutes / 60) % 24;
     let minutes = totalMinutes % 60;
     let newAmPm = hours < 12 ? 'AM' : 'PM';
@@ -77,16 +118,16 @@ function minutesToFormat(totalMinutes){
     } else if (hours === 0) {
         hours = 12;
     }
-    let resultStr = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ' ' + newAmPm; 
+    let resultStr = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ' ' + newAmPm;
     return resultStr;
 }
 
-function timeToMinutes(timeStr){
+function timeToMinutes(timeStr) {
     let timeParts = timeStr.split(/:| /);
     let hours = parseInt(timeParts[0], 10);
     let minutes = parseInt(timeParts[1], 10);
     let ampm = timeParts[3];
-    
+
     if (ampm === 'PM' && hours < 12) {
         hours += 12;
     }
